1da1aee4d274f9f7a60eaa5a605b48db65136fc2
Pavel Shemetov <pevel.shem@gmail.com>
2025-04-13 01:12:39 +0500

    add help

--- src/main/kotlin/apps/habits/bot/HabitsHandlers.kt
+++ src/main/kotlin/apps/habits/bot/HabitsHandlers.kt
@@ -3,12 +3,19 @@ package apps.habits.bot
 import bot.BotHandler
 import com.github.kotlintelegrambot.dispatcher.Dispatcher
 import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
 
 class HabitsHandlers: BotHandler {
     override fun registerHandlers(dispatcher: Dispatcher) {
         dispatcher.command("habits") {
             val chatId = ChatId.fromId(update.message?.chat?.id ?: return@command)
+            bot.sendMessage(chatId = chatId, text = "hello")
+        }
+
+        dispatcher.text("help"){
+            val chatId = ChatId.fromId(update.message?.chat?.id ?: return@text)
+
             bot.sendMessage(chatId = chatId, text = "hello")
         }
     }
